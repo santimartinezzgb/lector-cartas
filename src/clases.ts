a9864a27959bfb6ced3cf4edaaade0e7cc2b957d
smz7z <santimgb98@gmail.com>
2025-09-19 09:00:42 +0200

    clases.js  ->  clases.tsç

---
+++ src/clases.ts
@@ -0,0 +1,14 @@
+export class Monstruo {
+    nombre: string
+    tipo: string
+    fuerza: number
+    vida: number
+    defensa: number
+    constructor(nombre: string, tipo: string, fuerza: number, vida: number, defensa: number) {
+        this.nombre = nombre
+        this.tipo = tipo
+        this.fuerza = fuerza
+        this.vida = vida
+        this.defensa = defensa
+    }
+}
